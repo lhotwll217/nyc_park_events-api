991e80a52dcffe3c7759fcf1f323d0f282ada594
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-09 23:17:44 -0400

    Saved Events Updated Backend

--- app/controllers/saved_events_controller.rb
+++ app/controllers/saved_events_controller.rb
@@ -1,9 +1,9 @@
 class SavedEventsController < ApplicationController
 
 
-    def show
+    def index
 
-        saved_events = SavedEvent.where( user_id: params[user_id] )
+        saved_events = SavedEvent.where( user_id: session[:user_id] )
 
         if saved_events 
             render json: saved_events
@@ -19,6 +19,17 @@ class SavedEventsController < ApplicationController
             render json: saved_event, status: :created
         else
             render json: {errors: "Not Created"}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+
+        saved_event = SavedEvent.find_by(id: params[:id])
+        if saved_event
+            saved_event.destroy
+            render json: saved_event
+        else
+            render json: {errors: "Not Found"}, status: :not_found
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+get "/events", to: "events#index"
 
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
