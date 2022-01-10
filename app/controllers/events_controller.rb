91bae819ae2cf9a9dc5911206fcb0f871c979927
lhotwll217 <lukehenryotwell@gmail.com>
2022-01-10 09:12:09 -0500

    Searchable Queries Working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,11 +12,14 @@ class EventsController < ApplicationController
     end
 
     def filtered 
+    #    events=Event.where(nil)
         events = Event.where('end_date_time > ?',  DateTime.current).order('start_date_time ASC, id')
         events = events.filter_by_date(params[:date]) if params[:date].present?
+        events = events.where('title ILIKE :search OR categories ILIKE :search', search: "%#{params[:search]}%" ) if params[:search].present?
         events = events.page(params[:page]).per(10)
         if events
             render json: events, status: :ok
+            puts events
         else
             render json: {errors: "Not Found"}, status: :not_found
         end
