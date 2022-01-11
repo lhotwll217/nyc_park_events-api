3ac12677e1a0f55f550b9faccdcc8bdb9d554730
lhotwll217 <lukehenryotwell@gmail.com>
2022-01-10 22:49:18 -0500

    Controller Refactored

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,12 @@
 class EventsController < ApplicationController
 
-    def index 
 
-        events = Event.where('end_date_time > ?',  DateTime.current).order('start_date_time ASC').limit(1000)
-    
-        if events
-            render json: events, status: :ok
-        else
-            render json: {errors: "Not Found"}, status: :not_found
-        end
-    end
-
-    def filtered 
-    #    events=Event.where(nil)
+    def index
+        
         events = Event.where('end_date_time > ?',  DateTime.current).order('start_date_time ASC, id')
         events = events.filter_by_date(params[:date]) if params[:date].present?
         events = events.where('title ILIKE :search OR categories ILIKE :search', search: "%#{params[:search]}%" ) if params[:search].present?
-        events = events.page(params[:page]).per(10)
+        events = events.page(params[:page]).per(12)
         if events
             render json: events, status: :ok
             puts events
@@ -25,18 +15,6 @@ class EventsController < ApplicationController
         end
     end
 
-    def paginate
-
-        puts params[:page]
-
-        events = Event.where('end_date_time > ?',  DateTime.current).order('start_date_time ASC, id'
-        ).page(params[:page]).per(10)
-
-        if events
-            render json: events, status: :ok
-        else
-            render json: {errors: "Not Found"}, status: :not_found
-        end
-    end
+   
 
 end
