9339c927911a0007ce254e2d693e7406d17c1421
lhotwll217 <lukehenryotwell@gmail.com>
2022-01-20 12:21:51 -0500

    Spaced Search

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,20 @@ class EventsController < ApplicationController
 
 
     def index
+
+        if params[:search] && params[:search].include?(' ') 
+        spacedSearch= params[:search].split
+        
+      
+        end
         
         events = Event.where('end_date_time > ?',  DateTime.current).order('start_date_time ASC, id')
         events = events.filter_by_date(params[:date]) if params[:date].present?
-        events = events.where('title ILIKE :search OR categories ILIKE :search', search: "%#{params[:search]}%" ) if params[:search].present?
+        if spacedSearch
+            spacedSearch.each { |s| events = events.where('title ILIKE :search OR categories ILIKE :search', search: "%#{s}%" )}
+        else
+            events = events.where('title ILIKE :search OR categories ILIKE :search', search: "%#{params[:search]}%" ) if params[:search].present?
+        end
         events = events.page(params[:page]).per(12)
         if events
             render json: events, status: :ok
